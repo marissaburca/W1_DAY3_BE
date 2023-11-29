ea85c3d7a053abe2e093b2da88cdf6a1eda398e4
Marissa Burca <Marissaburca@gmail.com>
2023-11-29 16:41:48 +0100

    esercizio 1 fatto con scanner

--- src/first/Main.java
+++ src/first/Main.java
@@ -10,9 +10,9 @@ public class Main {
         System.out.println("Enter width:");
         int width= Integer.parseInt(userInput.nextLine());
 
-        Rettangolo rectangle1 = new Rettangolo(height,width);
-        int result1 = rectangle1.perimeter();
-        int result2 = rectangle1.area();
+        Rettangolo printRectangle = new Rettangolo(height,width);
+        int result1 = printRectangle.perimeter();
+        int result2 = printRectangle.area();
         System.out.println("The perimeter is equal to: "+ result1 +". The area is: "+ result2);
 
 
@@ -25,15 +25,15 @@ public class Main {
         System.out.println("Enter second width:");
         int width2= Integer.parseInt(userInput.nextLine());
 
-        Rettangolo rectangle2 = new Rettangolo(height1,width1,height2,width2);
-        int result3 = rectangle2.perimeter();
-        int result4 = rectangle2.area();
-        int result5 = rectangle2.perimeter1();
-        int result6 = rectangle2.area1();
-        int result7 = rectangle2.perimeterTotal();
-        int result8 = rectangle2.areaTotal();
+        Rettangolo printTwoRectangles = new Rettangolo(height1,width1,height2,width2);
+        int result3 = printTwoRectangles.perimeter();
+        int result4 = printTwoRectangles.area();
+        int result5 = printTwoRectangles.perimeter1();
+        int result6 = printTwoRectangles.area1();
+        int result7 = printTwoRectangles.perimeterTotal();
+        int result8 = printTwoRectangles.areaTotal();
         System.out.println("The first perimeter is equal to: "+ result3 +". The area is: "+ result4 +". The second perimeter is equal to: "+ result5 +". The area is: "+ result6 +". The perimeters sum is: "+ result7 + ". The areas sun is: " + result8);
 
-
+        userInput.close();
     }
 }
